15dd3df069d3b69f23e6f66b7f5d56fa8a10d9c2
Jessica Ciochetto <jessicaciochetto@gmail.com>
2023-11-04 19:10:06 -0500

    add enr entry parser

---
+++ lib/bank/ach/enr_entry.ex
@@ -0,0 +1,23 @@
+defmodule Bank.Ach.EnrEntry do
+  use Bank.Ach.LineParser
+
+  @impl Bank.Ach.LineParser
+  def parse!(text, line_number, opts) do
+    new!(text, line_number, opts)
+    |> put(:record_type, :enr_entry)
+    |> field!(:record_type_code, :M, "6", 1)
+    |> field!(:transaction_code, :M, :numeric, 2)
+    |> field!(:receiving_dfi_identification, :M, :routing_number, 8)
+    |> field!(:check_digit, :M, :numeric, 1)
+    |> field!(:dfi_account_number, :R, :alphameric, 17)
+    |> field!(:amount, :M, "0000000000", 10)
+    |> field!(:identification_number, :O, :alphameric, 15)
+    |> field!(:number_of_addenda_records, :M, :numeric, 4)
+    |> field!(:receiving_company_name, :R, :alphameric, 16)
+    |> field!(:reserved, :NA, :blank, 2)
+    |> field!(:discretionary_data, :O, :alphameric, 2)
+    |> field!(:addenda_record_indicator, :M, :numeric, 1)
+    |> field!(:trace_number, :M, :numeric, 15)
+    |> Map.get(:record)
+  end
+end
