a2295ce5a288d8d041911b5da24b87769ee95885
Jessica Ciochetto <139469061+ciochetto@users.noreply.github.com>
2023-11-07 21:45:39 -0600

    add pop entry parser (#59)

---
+++ lib/bank/ach/pop_entry.ex
@@ -0,0 +1,23 @@
+defmodule Bank.Ach.PopEntry do
+  use Bank.Ach.LineParser
+
+  @impl Bank.Ach.LineParser
+  def parse!(text, line_number, opts) do
+    new!(text, line_number, opts)
+    |> put(:record_type, :pop_entry)
+    |> field!(:record_type_code, :M, "6", 1)
+    |> field!(:transaction_code, :M, :numeric, 2)
+    |> field!(:receiving_dfi_identification, :M, :routing_number, 8)
+    |> field!(:check_digit, :M, :numeric, 1)
+    |> field!(:dfi_account_number, :R, :alphameric, 17)
+    |> field!(:amount, :M, :amount_in_cents, 10)
+    |> field!(:check_serial_number, :M, :alphameric, 9)
+    |> field!(:terminal_city, :M, :alphameric, 4)
+    |> field!(:terminal_state, :M, :alphameric, 2)
+    |> field!(:individual_name, :O, :alphameric, 22)
+    |> field!(:discretionary_data, :O, :alphameric, 2)
+    |> field!(:addenda_record_indicator, :M, :numeric, 1)
+    |> field!(:trace_number, :M, :numeric, 15)
+    |> Map.get(:record)
+  end
+end
